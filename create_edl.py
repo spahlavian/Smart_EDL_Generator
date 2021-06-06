7f5b5d1a38247ec09a20cc4f7d173f33b4cda498
Soroush Pahlavian <Soroush.heidari@gmail.com>
2021-06-06 17:45:12 -0400

    Initial commit

---
+++ create_edl.py
@@ -0,0 +1,14 @@
+import os
+import glob
+from pathlib import Path
+from matplotlib import pyplot as plt
+from moviepy.editor import VideoFileClip, AudioFileClip
+import numpy as np
+import librosa
+import random
+import math
+import colorsys
+
+
+def main():
+    pass
